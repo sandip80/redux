f5536c800b309dbe51ebc1115f2ad8b22795648c
sandip80 <sandip.samantray806@gmail.com>
2017-02-15 21:33:08 -0800

    Add sprites to menu state

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -4,18 +4,18 @@ module Redux {
     export class Game {
         game: Phaser.Game;
         aspectRatio: Number;
-        
+
         constructor() {
             this.game = new Phaser.Game(1600, 900, Phaser.AUTO, 'main-game', {
                preload: this.preload, create: this.create
             });
         }
-        
+
         preload() {
             // Spritesheet
             this.game.load.atlasXML('sprite_complete', 'assets/graphics/sprites/spritesheet_complete.png', 'assets/graphics/sprites/spritesheet_complete.xml');
             this.game.load.atlasXML('red_buttons', 'assets/graphics/sprites/redSheet.png', 'assets/graphics/sprites/redSheet.xml');
-            
+
             // Bacgrounds
             this.game.load.image('blue_desert', 'assets/graphics/backgrounds/blue_desert.png');
             this.game.load.image('blue_grass', 'assets/graphics/backgrounds/blue_grass.png');
@@ -25,12 +25,14 @@ module Redux {
             this.game.load.image('colored_grass', 'assets/graphics/backgrounds/colored_grass.png');
             this.game.load.image('colored_land', 'assets/graphics/backgrounds/colored_land.png');
             this.game.load.image('colored_shroom', 'assets/graphics/backgrounds/colored_shroom.png');
-            
+
             // Sounds
         }
-        
+
         create() {
             this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            this.game.world.enableBody = true;
             if (window.innerWidth > 768 && window.innerWidth / window.innerHeight > 1) {
                 this.game.state.add('Menu', ReduxMain.Menu, true);
             } else {
@@ -41,5 +43,4 @@ module Redux {
 }
 
 // export Game to window
-var Game = Redux.Game;
-
+const Game = Redux.Game;
--- src/scripts/state/menu.ts
+++ src/scripts/state/menu.ts
@@ -5,33 +5,115 @@ module ReduxMain {
         game: Phaser.Game;
         background: Phaser.TileSprite;
         debugText: Phaser.Text;
+        title: Phaser.Text;
+        entity: Phaser.Sprite;
+        map = [
+            '--------------------------------------------------',
+            '                                                  ',
+            '                                                  ',
+            '                                                  ',
+            '                                                  ',
+            '                   _    _          = = =     _    ',
+            '    )(       )====                 + + +  s   )==(',
+            '====/\\]  [] [/++++  _ _ _ [] [] [] + + + == [=/++\\=',
+            '--------------------------------------------------'
+        ];
+        // length of the map = 50 blocks, width of the map = 9
+        tileMap = {};
 
         constructor() {
             super();
         }
-        
+
         create() {
-            this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'colored_desert');
-            this.background.tileScale.set(this.background.height / window.innerHeight, this.background.height / window.innerHeight);
-            this.debugText = this.game.add.text(0, 0, this.createDebugString(), {font: "32px Arial", fill: '#32cde0', wordWrap: true, wordWrapWidth: this.game.width, align: 'center'});
-            // this.background.scale.setTo(this.game.width / this.background.width, this.game.height / this.background.height);
-            // var spriteComplete = this.game.add.sprite((window.innerWidth - 128) / 2, (this.background.height - 256) / 2, 'sprite_complete');
-            // spriteComplete.animations.add('play', Phaser.Animation.generateFrameNames('alienBlue_walk', 1, 2, '.png'), 7, true);
-            // spriteComplete.animations.play('play');
+            this.game.world.setBounds(0, 0, 6400, 896);
+            this.background = this.game.add.tileSprite(0, -window.innerHeight / 10, this.game.width * 5, this.game.height * 2, 'colored_desert');
+            this.title = this.game.add.text(this.game.width / 2 + this.game.width * 3, this.game.height/2, 'Redux', { font: "32px Arial" });
+            this.createMap();
+            this.entity = this.game.add.sprite(this.game.width / 2, this.game.height / 2, '');
+            //this.game.camera.follow(this.entity);
+            this.game.camera.x += this.game.width * 3;
+            var worm = this.game.add.sprite(this.game.width * 3, 0, 'sprite_complete');
+            worm.animations.add('move', Phaser.Animation.generateFrameNames('torch', 1, 2, '.png', 1), 4, true);
+            worm.animations.play('move');
+            //Debug:
+            //this.debugText = this.game.add.text(0, 0, this.createDebugString(), {font: "32px Arial", fill: '#32cde0', wordWrap: true, wordWrapWidth: this.game.width, align: 'center'});
+
         }
-        
+
+        createMap() {
+            for (let i = 0; i < this.map.length; i++) {
+                for (let j = 0; j < this.map[i].length; j++) {
+                    if (this.map[i][j] == '-' || this.map[i][j] == '+') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassCenter.png', true);
+                    } else if (this.map[i][j] == '=') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassMid.png', true);
+                    } else if (this.map[i][j] == ']') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassRight.png', true);
+                    } else if (this.map[i][j] == '[') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassLeft.png', true);
+                    } else if (this.map[i][j] == '/') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassCorner_right.png', true);
+                    } else if (this.map[i][j] == '\\') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassCorner_left.png', true);
+                    } else if (this.map[i][j] == '(') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassHill_left.png', true);
+                    } else if (this.map[i][j] == ')') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grassHill_right.png', true);
+                    } else if (this.map[i][j] == '_') {
+                        this.createTileOnMap(0 + 128 * j, -128 + 128 * i, 'sprite_complete', 'grass.png', true);
+                    }
+                }
+            }
+        }
+
+        createTileOnMap(x, y, spriteSheet, spriteFrame, immovable) {
+            var tile = this.game.add.sprite(x, y, spriteSheet);
+            tile.frameName = spriteFrame;
+            tile.body.immovable = immovable;
+            if (this.tileMap[spriteSheet] == null) {
+                this.tileMap[spriteSheet] = {};
+            } else if (this.tileMap[spriteSheet][spriteFrame] == null) {
+                this.tileMap[spriteSheet][spriteFrame] = {};
+            } else if (this.tileMap[spriteSheet][spriteFrame][x] == null) {
+                this.tileMap[spriteSheet][spriteFrame][x] = {};
+            } else {
+                this.tileMap[spriteSheet][spriteFrame][x][y] = tile;
+            }
+        }
+
+        beautifyText(text) {
+
+        }
+
         update() {
-            this.background.tilePosition.x += 2;
-            this.debugText.setText(this.createDebugString());
+            // for (let spriteSheet in this.tileMap) {
+            //     for (let sprite in this.tileMap[spriteSheet]) {
+            //         for (let x in this.tileMap[spriteSheet][sprite]) {
+            //             for (let y in this.tileMap[spriteSheet][sprite][x]) {
+            //                 console.log(this.tileMap[spriteSheet][sprite][x][y]);
+            //                 this.tileMap[spriteSheet][sprite][x][y].x -= 1;
+            //             }
+            //         }
+            //     }
+            // }
+            // this.entity.x += 0;
+            // this.title.x += 0;
+            // this.game.world.wrap(this.entity, -(this.game.width / 2), false, true, false);
+            // this.game.world.wrap(this.title, -(this.game.width / 2), false, true, false);
+            this.background.tilePosition.x -= 2;
+
+            //Debug:
+            //this.debugText.setText(this.createDebugString());
         }
 
         createDebugString() {
-            return(
-                "Game properties:\n" + 
-                "Width: " + this.game.width + "\n" + 
+            return (
+                "Game properties:\n" +
+                "Width: " + this.game.width + "\n" +
                 "Height: " + this.game.height + "\n" +
                 "Background properties:\n" +
-                "Width: " + this.background.width + "\n" + 
+                "Width: " + this.background.width + "\n" +
                 "Height: " + this.background.height
             );
         }
